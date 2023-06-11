969b7988e71a6816658b67b126c5f5c4dfce78ef
Shlomi Hafif <Shlomixg@gmail.com>
2023-06-11 14:23:11 +0300

    add macro inner join with buckets

--- dbt/include/clickhouse/macros/utils/utils.sql
+++ dbt/include/clickhouse/macros/utils/utils.sql
@@ -57,3 +57,21 @@
 {% macro clickhouse__array_concat(array_1, array_2) -%}
    arrayConcat({{ array_1 }}, {{ array_2 }})
 {% endmacro %}
+
+{% macro clickhouse__inner_join_with_buckets(table_1, table_2, join_bucket_alias, table_2_keys, join_on_clause='', num_of_buckets=16) -%}
+    {%- for bucket in range(num_of_buckets) %}
+    select *
+    from {{ table_1 }}
+    join (
+        select
+            *,
+            cityHash64({{ table_2_keys }}) % {{ num_of_buckets }} as bucket_num
+        from {{ table_2 }}
+        where bucket_num = {{ bucket }}
+    ) as {{ join_bucket_alias }} on 
+    {{ join_on_clause }}
+    {% if not loop.last %}
+    union all
+    {% endif %}
+    {%- endfor %}
+{% endmacro %}
